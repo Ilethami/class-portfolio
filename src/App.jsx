80d5afc606ea2891aafc9d1defb92a646a9e95c2
Ilethami <elijohn072008@gmail.com>
2026-08-26 23:40:46 +0800

    Reversed back to base

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Idk</h1>
+          <h1>Get started</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test{" "}
             <code>HMR</code>
